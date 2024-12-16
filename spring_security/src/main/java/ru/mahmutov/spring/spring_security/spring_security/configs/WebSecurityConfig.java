1702eb627f19b01740c3c2dd40e2a60e428d3b1c
Mahmutov Azat <cudraviymalay@gmail.com>
2024-12-16 18:57:37 +0300

    добавил конфиг, надо докрутить логику

---
+++ spring_security/src/main/java/ru/mahmutov/spring/spring_security/spring_security/configs/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package ru.mahmutov.spring.spring_security.spring_security.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final SuccessUserHandler successUserHandler;
+
+    @Autowired
+    public WebSecurityConfig(SuccessUserHandler successUserHandler) {
+        this.successUserHandler = successUserHandler;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/index").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login -> login
+                        .successHandler(successUserHandler)
+                        .permitAll()
+                )
+                .logout(logout -> logout.permitAll())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
